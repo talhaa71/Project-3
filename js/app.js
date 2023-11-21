fa13442991a0bb29eb7aa4c97a997bb461e125bd
talhaa71 <143561482+talhaa71@users.noreply.github.com>
2023-11-20 23:36:50 -0600

    added and fixed slider, had issues with slider button, added function .onload

--- js/app.js
+++ js/app.js
@@ -3,5 +3,43 @@ function initMap(){
   let map = new google.maps.Map(
     document.getElementById('map'), {zoom: 12, center: mtPoint}
   );
-  let marker = new google.maps.Marker({position: mtPoint, map: map})
+  let marker = new google.maps.Marker({position: mtPoint, map: map});
 }
+
+
+window.onload = function () {
+  const swiper = new Swiper('.swiper', {
+    // Other parameters
+
+    loop: true,
+
+    pagination: {
+      el: '.swiper-pagination',
+    },
+
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
+};
+/*
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // And if we need scrollbar
+  
+});*/
